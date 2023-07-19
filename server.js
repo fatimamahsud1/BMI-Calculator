5dff8315215c0d0e82e10010da7a5a74280c1cd1
Fatimamahsud <fatimamahsud1@gamil.com>
2023-07-19 12:28:33 +0500

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const app = express()
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/index.html")
+})
+
+
+app.post('/', function(req, res) {
+    var mass = parseInt(req.body.mass);
+    var height = parseInt(req.body.height);
+    var result = mass/(height*height);
+    res.send("<h1>The result of the addition is: </h1>" + result);
+  })
+
+app.listen(3000)
